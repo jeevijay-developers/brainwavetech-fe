120322f4d9d9fa18785572f7f8c72396c6241650
icodelakshay <lakshaysaxena2217@gmail.com>
2025-11-03 11:35:23 +0530

    chore: Update featuresData.js for improved feature descriptions and consistency

--- src/constant/home2/featuresData.js
+++ src/constant/home2/featuresData.js
@@ -1,104 +1,101 @@
 const excellentFeaturesData = [
-    {
-        id: 1,
-        title: "Real-Time Brainwave Mapping",
-        p1: "Get instant insights into your alpha, beta, gamma, delta, and theta patterns for a clear picture of your mental state",
-        icon: "/images/Icons/task-list-icon.svg",
-        varient: 'bg-varient-1',
-    },
-    {
-        id: 2,
-        title: "Personalized Sound Therapy",
-        p1: " Experience healing frequencies tailored to your unique brainwave profile and emotional needs.",
-        icon: "/images/Icons/bell-icon.svg",
-        varient: 'bg-varient-1',
-    },
-    {
-        id: 3,
-        title: "Scientific & Non-Invasive Approach",
-        p1: "Backed by neuroscience, EEG technology, and vibrational medicine—safe, accurate, and proven.",
-        icon: "/images/Icons/agenda-icon.svg",
-        varient: 'bg-varient-1'
-    },
-    {
-        id: 4,
-        title: "Actionable Wellness Insights",
-        p1: "Receive practical steps to improve focus, sleep, creativity, and emotional resilience based on your brain profile.",
-        icon: "/images/Icons/agenda-icon.svg",
-        varient: 'bg-varient-1'
-    },
-    {
-        id: 5,
-        title: "Trackable Growth & Transformation",
-        p1: "Monitor measurable improvements in clarity, productivity, and emotional balance over time.",
-        icon: "/images/Icons/agenda-icon.svg",
-        varient: 'bg-varient-1'
-    },
-    {
-        id: 6,
-        title: "Expert Guidance & Ongoing Support",
-        p1: "Get professional interpretation of results and continuous support on your mental wellness journey.",
-        icon: "/images/Icons/agenda-icon.svg",
-        varient: 'bg-varient-1',
-    },
-]
-
+  {
+    id: 1,
+    title: "Real-Time Brainwave Mapping",
+    p1: "Get instant insights into your alpha, beta, gamma, delta, and theta patterns for a clear picture of your mental state",
+    icon: "/images/Icons/task-list-icon.svg",
+    varient: "bg-varient-1",
+  },
+  {
+    id: 2,
+    title: "Personalized Sound Therapy",
+    p1: " Experience healing frequencies tailored to your unique brainwave profile and emotional needs.",
+    icon: "/images/Icons/bell-icon.svg",
+    varient: "bg-varient-1",
+  },
+  {
+    id: 3,
+    title: "Scientific & Non-Invasive Approach",
+    p1: "Backed by neuroscience, EEG technology, and vibrational medicine—safe, accurate, and proven.",
+    icon: "/images/Icons/dashbord-icon.svg",
+    varient: "bg-varient-1",
+  },
+  {
+    id: 4,
+    title: "Actionable Wellness Insights",
+    p1: "Receive practical steps to improve focus, sleep, creativity, and emotional resilience based on your brain profile.",
+    icon: "/images/Icons/tracker-icon.svg",
+    varient: "bg-varient-1",
+  },
+  {
+    id: 5,
+    title: "Trackable Growth & Transformation",
+    p1: "Monitor measurable improvements in clarity, productivity, and emotional balance over time.",
+    icon: "/images/Icons/calendar-icon-2.svg",
+    varient: "bg-varient-1",
+  },
+  {
+    id: 6,
+    title: "Expert Guidance & Ongoing Support",
+    p1: "Get professional interpretation of results and continuous support on your mental wellness journey.",
+    icon: "/images/Icons/agenda-icon.svg",
+    varient: "bg-varient-1",
+  },
+];
 
 const singleFeature2 = {
-    features: [
-        {
-            id: 1,
-            title: "Automatic Brain Wave Tracking",
-            p: "Effortlessly capture and record your unique brain wave patterns with advanced technology—no manual input required.",
-            icon: "/images/Icons/tracker-icon.svg"
-        },
-        {
-            id: 2,
-            title: "Customizable Insights Dashboard",
-            p: "Personalize your brain performance dashboard to focus on the areas that matter most—learning, emotional balance, or focus.",
-            icon: "/images/Icons/dashbord-icon.svg",
-            varient: "varient-1",
-        },
-        {
-            id: 3,
-            title: "Historical Brain Data",
-            p: "Access detailed past brain wave activity and trends to analyze performance, track emotional states, and optimize future growth.",
-            icon: "/images/Icons/calendar-icon-2.svg",
-            varient: "varient-2",
-        },
-    ]
-}
+  features: [
+    {
+      id: 1,
+      title: "Automatic Brain Wave Tracking",
+      p: "Effortlessly capture and record your unique brain wave patterns with advanced technology—no manual input required.",
+      icon: "/images/Icons/tracker-icon.svg",
+    },
+    {
+      id: 2,
+      title: "Customizable Insights Dashboard",
+      p: "Personalize your brain performance dashboard to focus on the areas that matter most—learning, emotional balance, or focus.",
+      icon: "/images/Icons/dashbord-icon.svg",
+      varient: "varient-1",
+    },
+    {
+      id: 3,
+      title: "Historical Brain Data",
+      p: "Access detailed past brain wave activity and trends to analyze performance, track emotional states, and optimize future growth.",
+      icon: "/images/Icons/calendar-icon-2.svg",
+      varient: "varient-2",
+    },
+  ],
+};
 
 const singleFeature3 = {
-    graph: {
-        img1: "/images/working/working-img.jpeg",
-        img2: "/images/working/working-image1.jpeg",
-        img3: "/images/working/girl-image.jpeg",
+  graph: {
+    img1: "/images/working/working-img.jpeg",
+    img2: "/images/working/working-image1.jpeg",
+    img3: "/images/working/girl-image.jpeg",
+  },
+  features: [
+    {
+      id: 1,
+      title: "Quick & Non-Invasive Reading",
+      p: "A gentle MEG headset scans your brain activity in just 2 minutes, providing instant results without discomfort.",
+      icon: "/images/Icons/min-icon-xl.svg",
     },
-    features: [
-        {
-            id: 1,
-            title: "Quick & Non-Invasive Reading",
-            p: "A gentle MEG headset scans your brain activity in just 2 minutes, providing instant results without discomfort.",
-            icon: "/images/Icons/min-icon-xl.svg"
-        },
-        {
-            id: 2,
-            title: "15 Key Mind Parameters",
-            p: "From Alpha, Beta, Gamma waves to Joy, Inner Calm, Creativity, and more—track 15 scientifically validated mental states in a clear, graphical format.",
-            icon: "/images/Icons/hourglass-icon-xl.svg",
-            varient: "varient-1",
-        },
-        {
-            id: 3,
-            title: "Personalized Insights for Growth",
-            p: "Identify the areas most important for your life and receive tailored guidance to enhance focus, peace, and overall well-being.",
-            icon: "/images/Icons/wallet-icon-xl.svg",
-            varient: "varient-2",
-        },
-    ]
-}
+    {
+      id: 2,
+      title: "15 Key Mind Parameters",
+      p: "From Alpha, Beta, Gamma waves to Joy, Inner Calm, Creativity, and more—track 15 scientifically validated mental states in a clear, graphical format.",
+      icon: "/images/Icons/hourglass-icon-xl.svg",
+      varient: "varient-1",
+    },
+    {
+      id: 3,
+      title: "Personalized Insights for Growth",
+      p: "Identify the areas most important for your life and receive tailored guidance to enhance focus, peace, and overall well-being.",
+      icon: "/images/Icons/wallet-icon-xl.svg",
+      varient: "varient-2",
+    },
+  ],
+};
 
-
-
-export { excellentFeaturesData, singleFeature2, singleFeature3 }
+export { excellentFeaturesData, singleFeature2, singleFeature3 };
